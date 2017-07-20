63cc1168c3b70b8c2fb4207f4b0a1d9bae514fec
shmirASC4 <sarkers1@bxscience.edu>
2017-07-20 13:49:13 -0400

    update a lot of stuff

--- pokemonBattle/battle.js
+++ pokemonBattle/battle.js
@@ -75,7 +75,83 @@ pikachu.moves.push(pikamovelist);
 console.log(p1);
 console.log(comp);
 
-comp[0].hp= comp[0].hp - p1[0].moves[0][1].dmg;
+// comp[0].hp= comp[0].hp - p1[0].moves[0][1].dmg;
 console.log(p1[0].name, "used", p1[0].moves[0][1].name, "on", comp[0].name);
 console.log(comp[0]);
+
+function draw(){
+
+   text(String(charizard.name), 600, 400);
+    text(String(charizard.hp), 600, 425);
+    text(String(pikachu.name), 50, 25);
+    text(String(pikachu.hp), 50, 50);
+    if(p1[0].hp <=0 || comp[0].hp <=0){
+    alert("Game over");
+}
+
+
+}
+
+
+var check;
+
+function keyPressed(){
+    
+   var cm = Math.round(Math.random() * 3);
+
+   check = true;
+
+    if (check == true){
+        p1[0].hp = p1[0].hp - comp[0].moves[0][cm].dmg;
+    }
+
+   b = key;
+    comp[0].hp = comp[0].hp - p1[0].moves[0][b-1].dmg;
+    clear()
+
+   createCanvas(1000, 1000);
+    background(150);
+
+  fill("LightCoral");
+    rect(50,25,150,50);
+
+  fill("CornflowerBlue");
+    rect(600,400,150,50);
+
+   textSize(20);
+    
+   //button 1
+    fill("Yellow");
+    rect(20,530,150,50);
+    fill("Black");
+    text("Flamethrower", 32,545,150,50);
+
+  //button 2
+    fill("Yellow");
+    rect(200,530,150,50);
+    fill("Black");
+    text("Fly", 270,545,150,50);
+
+  //button 3
+    fill("Yellow");
+    rect(370,530,150,50);
+    fill("Black");
+    text("Slash", 410,545,150,50);
+
+  //button 4
+    fill("Yellow");
+    rect(530,530,150,50);
+    fill("Black");
+    text("Ember", 570,545,150,50);
+
+
+   text(String(charizard.name), 600, 400);
+    text(String(charizard.hp), 600, 425);
+    text(String(pikachu.name), 50, 25);
+    text(String(pikachu.hp), 50, 50);
+
+   
+
+   return false;
+}
 
