b4db3b9b99848a8a6f994ae3c3ae0117ba45ce29
shmirASC4 <sarkers1@bxscience.edu>
2017-08-16 11:33:50 -0400

    new things

---
+++ FinalProject/mobile.js
@@ -0,0 +1,21 @@
+//called when successful user log in
+function onSignIn(googleUser) {
+    console.log('User signed in!');
+    var profile = googleUser.getBasicProfile();
+    //change userName text, img source, & email text based on profile
+    $(".userName").text(profile.getName());
+    $(".email").text(profile.getEmail());
+    window.location.href = "user.html";
+}
+
+//called when "sign out" button clicked
+function onSignOut() {
+    //should sign user out and toggleHidden
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.')
+        //setting back to default
+        $(".userName").text("USER_NAME");
+        $(".email").text("example@example.com");
+    });
+}
